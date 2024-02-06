2429d820c09963839df5fc494f48c927aa9300cc
Willian Sato <satowillianmassayuki@gmail.com>
2024-02-05 23:58:06 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import Logo from "./assets/logo-nlw-experts.svg";
+import { NewNoteCard } from "./components/new-note-card";
+import { NoteCard } from "./components/note-card";
+
+export function App(): any {
+  return (
+    <div className="mx-auto my-12 max-w-4xl space-y-4">
+      <img src={Logo} alt="NLW Expert" />
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
+        />
+      </form>
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+      </div>
+    </div>
+  );
+}
